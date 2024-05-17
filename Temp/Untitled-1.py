ade714b2bf59fb459cf2ae531b3494e4f4773292
ILVACM <3347620766@qq.com>
2024-05-17 19:34:23 +0800

    add

--- Temp/Untitled-1.py
+++ Temp/Untitled-1.py
@@ -15,31 +15,50 @@ data.shape
 
 data.dtypes
 
+data = data[['time', 'status', 'age', 'sex', 'ph.ecog', 'ph.karno','pat.karno', 'meal.cal', 'wt.loss']]
+data["status"] = data["status"] - 1
+data["sex"] = data["sex"] - 1
+data.head()
+
 data.isnull().sum()
+
+data.columns
 
 # 填补缺失值
 data["ph.karno"].fillna(data["ph.karno"].mean(), inplace=True)
 data["pat.karno"].fillna(data["pat.karno"].mean(), inplace=True)
 data["meal.cal"].fillna(data["meal.cal"].mean(), inplace=True)
 data["wt.loss"].fillna(data["wt.loss"].mean(), inplace=True)
-
 data.dropna(inplace=True)
-data.isnull().sum()
 
 data["ph.ecog"] = data["ph.ecog"].astype("int64")
 
-E = data['status']
+data.isnull().sum()
+data.shape
 
 T = data["time"]
+E = data["status"]
 plt.hist(T, bins = 50)
 plt.show()
 
+#Fitting a non-parametric model [Kaplan Meier Curve]
 kmf = KaplanMeierFitter()
 kmf.fit(durations=T, event_observed=E)
 kmf.plot_survival_function()
+
+kmf.survival_function_.plot()
 plt.title('Survival function')
 
+kmf.plot_cumulative_density()
+
 kmf.median_survival_time_
+
+from lifelines.utils import median_survival_times
+
+median_ = kmf.median_survival_time_
+median_confidence_interval_ = median_survival_times(kmf.confidence_interval_)
+print(median_)
+print(median_confidence_interval_)
 
 ax = plt.subplot(111)
 m = (data["sex"] == 0)
